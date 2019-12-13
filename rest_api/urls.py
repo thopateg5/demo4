5b24e3a9581215f89e9d648505745c10eefdad7c
Rohit <rohit.dhembare@softnautics.com>
2019-12-13 10:48:31 +0530

    right url updated

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,26 +1,42 @@
+
+#from rest_framework import routers
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-# from .views import CreateView 
-# from .views import DeleteView
-from .views import IoTHub_Device 
-from .views import Get_All_Device
-from .views import  Delete_All_Device
-from rest_framework import routers
+from .views import Selected_Device 
+from .views import All_Device
 
-
-# router = routers.DefaultRouter()
-# router.register(r'bucketlist',DeleteView)
 urlpatterns = {
-   # url(r'^bucketlists/$', CreateView.as_view(), name="create"),
-    #url(r'^list/', ListAPIView.as_view(), name="create1"),
-    #url(r'^bucketlist/delete/(?P<pk>[0-9]+)/$', DeleteView.as_view(), name="delete"),
-   # url(r'^bucketlists/delete/(?P<pk>[0-9]+)/$',DeleteView.as_view(), name="Delete"),
-    url(r'^bucketlists/IoTHub_Device/',IoTHub_Device.as_view(), name="Delete"),  
-    url(r'^bucketlists/Get_All_Device/',Get_All_Device.as_view(), name="list"), 
-    url(r'^bucketlists/Delete_All_Device/',Delete_All_Device.as_view(), name="list"), 
-   # url('bucketlists/', include('rest_framework.urls', namespace='rest_framework'))
-
-
+    url(r'^Selected_Device/',Selected_Device.as_view(), name="Delete"),  
+    url(r'^All_Device/',All_Device.as_view(), name="list")     
 }
     
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# urlpatterns = {
+#    # url(r'^bucketlists/$', CreateView.as_view(), name="create"),
+#     #url(r'^list/', ListAPIView.as_view(), name="create1"),
+#     #url(r'^bucketlist/delete/(?P<pk>[0-9]+)/$', DeleteView.as_view(), name="delete"),
+#    # url(r'^bucketlists/delete/(?P<pk>[0-9]+)/$',DeleteView.as_view(), name="Delete"),
+#     url(r'^bucketlists/Selected_Device/',Selected_Device.as_view(), name="Delete"),  
+#     url(r'^bucketlists/All_Device/',All_Device.as_view(), name="list"), 
+#     #url(r'^bucketlists/Delete_All_Device/',Delete_All_Device.as_view(), name="list"), 
+#    # url('bucketlists/', include('rest_framework.urls', namespace='rest_framework'))
+
+
+# }
--- rest_api/views.py
+++ rest_api/views.py
@@ -1,60 +1,64 @@
-from rest_framework import generics
 #from .serializers import BucketlistSerializer
 #from .models import Bucketlist
-from .devicemanager import DeviceManager
 #import requests
 #from django.http import QueryDict
+from rest_framework import generics
+from .devicemanager import DeviceManager
 import json
 from django.http import HttpResponse
 
 connectionString = 'HostName=newmyiothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=7m1wTjgz4bgjPtTpRoV/fLhH3m73o9j9J0qtaJ9DJSU='
 dm = DeviceManager(connectionString)	
 
-class IoTHub_Device(generics.RetrieveDestroyAPIView):
+class Selected_Device(generics.RetrieveDestroyAPIView):
 
     def delete(self,request):
         person_dict = json.loads(request.body)
-        #print(person_dict['name'])
         deviceId = person_dict['name']           
         responce_from_azure = dm.deleteDeviceId(deviceId)
         return  HttpResponse(responce_from_azure)
         
         
     def post(self, request):
-        #print(request.body)
         person_dict = json.loads(request.body)       
         deviceId = person_dict['name']          
         responce_from_azure = dm.createDeviceId(deviceId)        
-        return  HttpResponse(responce_from_azure)
-       
+        return  HttpResponse(responce_from_azure)   
 
             
     def get(self, request):
-        #print(request.body)
         person_dict = json.loads(request.body)       
         deviceId = person_dict['name']          
         responce_from_azure = dm.retrieveDeviceId(deviceId)    # dm.listDeviceIds()    
         return HttpResponse(responce_from_azure)
  
        
-class Get_All_Device(generics.ListAPIView):
+class All_Device(generics.ListAPIView):
     
-    def get(self, request):     
-        #print(request.body)
-        person_dict = json.loads(request.body)       
-        deviceId = person_dict['name']          
+    def get(self, request): 
         responce_from_azure = dm.listDeviceIds()     #dm.retrieveDeviceId(deviceId)  
         return HttpResponse(responce_from_azure)
-    
 
-class Delete_All_Device(generics.RetrieveDestroyAPIView):
-    
-    def delete(self, request):     
-        #print(request.body)
-        person_dict = json.loads(request.body)       
-        deviceId = person_dict['name']          
+    def delete(self, request):
         responce_from_azure = dm.DeletelistDevice() 
         return HttpResponse(responce_from_azure)
+
+
+
+
+
+
+
+
+
+# class Delete_All_Device(generics.RetrieveDestroyAPIView):
+    
+#     def delete(self, request):     
+#         #print(request.body)
+#         person_dict = json.loads(request.body)       
+#         deviceId = person_dict['name']          
+#         responce_from_azure = dm.DeletelistDevice() 
+#         return HttpResponse(responce_from_azure)
 
 
 # class CreateView(generics.ListCreateAPIView):
