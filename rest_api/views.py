1a791900f224f275927106ac68ddfd5a72040e08
Rohit <rohit.dhembare@softnautics.com>
2019-12-16 11:44:54 +0530

    first commit from rohit

--- rest_api/views.py
+++ rest_api/views.py
@@ -6,9 +6,14 @@ from rest_framework import generics
 from .devicemanager import DeviceManager
 import json
 from django.http import HttpResponse
+from .datamasemanagement import DatabaseManagement
 
-connectionString = 'HostName=newmyiothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=7m1wTjgz4bgjPtTpRoV/fLhH3m73o9j9J0qtaJ9DJSU='
-dm = DeviceManager(connectionString)	
+
+#connectionString = 'HostName=newmyiothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=7m1wTjgz4bgjPtTpRoV/fLhH3m73o9j9J0qtaJ9DJSU='
+connectionString = 'HostName=demo4IoTHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=/jbP/V4KmH/hvg7FvmTcaVMKp2sBZVsGPn1dwR8pPMo='
+
+dm = DeviceManager(connectionString)
+dm1 = DatabaseManagement()
 
 class Selected_Device(generics.RetrieveDestroyAPIView):
 
@@ -33,7 +38,7 @@ class Selected_Device(generics.RetrieveDestroyAPIView):
         return HttpResponse(responce_from_azure)
  
        
-class All_Device(generics.ListAPIView):
+class All_Device(generics.RetrieveDestroyAPIView):
     
     def get(self, request): 
         responce_from_azure = dm.listDeviceIds()     #dm.retrieveDeviceId(deviceId)  
@@ -42,6 +47,49 @@ class All_Device(generics.ListAPIView):
     def delete(self, request):
         responce_from_azure = dm.DeletelistDevice() 
         return HttpResponse(responce_from_azure)
+
+
+class less_than_temp(generics.ListAPIView):
+    
+    def get(self, request): 
+        person_dict = json.loads(request.body)       
+        deviceId = person_dict['value']  
+        responce_from_azure = dm1.temp_less_than(deviceId) 
+        #responce_from_azure = dm.listDeviceIds()     #dm.retrieveDeviceId(deviceId)  
+        return HttpResponse(responce_from_azure)
+
+class greater_than_temp(generics.ListAPIView):
+    
+    def get(self, request): 
+        person_dict = json.loads(request.body)       
+        vlaue = person_dict['value']  
+        responce_from_azure = dm1.temp_greater_than(vlaue) 
+        #responce_from_azure = dm.listDeviceIds()     #dm.retrieveDeviceId(deviceId)  
+        return HttpResponse(responce_from_azure)
+
+class between_humidity(generics.ListAPIView):
+
+    def get(self, request): 
+        person_dict = json.loads(request.body)       
+        min_val = person_dict['low'] 
+        max_val = person_dict['high'] 
+        responce_from_azure = dm1.humidity_in_between(min_val, max_val) 
+        #responce_from_azure = dm.listDeviceIds()     #dm.retrieveDeviceId(deviceId)  
+        return HttpResponse(responce_from_azure)
+
+class between_time(generics.ListAPIView):
+    
+    def get(self, request): 
+        person_dict = json.loads(request.body)       
+        min_val = person_dict['low'] 
+        max_val = person_dict['high'] 
+        responce_from_azure = dm1.between_time(min_val, max_val) 
+        #responce_from_azure = dm.listDeviceIds()     #dm.retrieveDeviceId(deviceId)  
+        return HttpResponse(responce_from_azure)
+
+
+
+  
 
 
 
